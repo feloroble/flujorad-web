c84b34b2cacde0fa08f367ab303e8653a6b92b92
Felix Roble <felix.roble@tecnotactil.com>
2025-05-13 23:41:37 +0200

    update

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template,url_for
 from . import auth_bp
 
 @auth_bp.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('auth/login.html')
+
+
+@auth_bp.route('/registro')
+def registro():
+    return render_template('auth/registro.html')
