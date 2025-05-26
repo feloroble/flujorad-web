770e0e3001a2ac9ace09f9de0206da422949c194
Felix Roble <felix.roble@tecnotactil.com>
2025-05-26 22:53:40 +0200

    hello

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     """Configuración general de la aplicación Flask."""
-    SECRET_KEY = os.getenv('SECRET_KEY', 'ghchfhfty5675756372h1f2h161725672562715627ft2thjft1r27527152715628175268.,-,...,.m.m../*.-*m,-.*m')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'ghjhghj')
     DB_NAME = os.getenv('DB_NAME', 'mi_bd_flask')
     DB_USER = os.getenv('DB_USER', 'usuario')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
