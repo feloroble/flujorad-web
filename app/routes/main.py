efc92060d23922710d678d77b367ba7adc096d77
Felix Roble <felix.roble@tecnotactil.com>
2025-06-05 01:21:04 +0200

    ajustes hechos

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,5 +5,12 @@ main_bp = Blueprint('main',  __name__)
 @main_bp.route('/')
 def home():
 
-    
     return render_template('index.html')
+
+@main_bp.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@main_bp.route('/acerca-de')
+def acerca_de():
+    return render_template('acerca_de.html')
