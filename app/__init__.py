e906e8c8dc6485853af0dbcd7ff95095fa8c46d2
Felix Roble <felix.roble@tecnotactil.com>
2025-07-17 18:51:59 +0200

    update del inicio de la app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask
+import click
+from flask import Flask, render_template
 from dotenv import load_dotenv
+from flask.cli import with_appcontext
 from app.utils.mail import init_mail
-from .extensions import db ,migrate , bcrypt, login_manager,csrf
-from .models import register_models  # carga dinámica
-from .routes import register_routes
-
+from app.extensions import db, migrate, bcrypt, login_manager, csrf
+from app.models import register_models
+from app.routes import register_routes
 
 load_dotenv()
 
@@ -17,12 +18,13 @@ def create_app():
 
     # Inicializar extensiones
     db.init_app(app)
-    migrate.init_app(app, db)  # <- IMPORTANTE
+    migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)
+    init_mail(app)
 
-      # Configuración de correo con variables de entorno
+    # Configuración correo
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'mail.privateemail.com')
     app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 465))
     app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False').lower() == 'true'
@@ -33,29 +35,32 @@ def create_app():
         os.getenv('MAIL_SENDER_NAME', 'Tecno Táctil'),
         os.getenv('MAIL_USERNAME')
     )
-    init_mail(app)
-   
-   # Cargar modelo User para LoginManager
+
+    # Registrar modelos
+    with app.app_context():
+        register_models()
+        db.create_all()
+
+    # Login manager
     from app.models.user import User
     @login_manager.user_loader
     def load_user(user_id):
         return db.session.get(User, int(user_id))
-
     login_manager.login_view = 'auth.login'
 
-   
-    # Registrar modelos automáticamente
-    with app.app_context():
-        register_models()
-        db.create_all() # Crea las tablas en la base de datos
-        
-    # Registrar rutas
+    # Rutas
     register_routes(app)
 
-    from flask import render_template
-
+    # Página de error 404
     @app.errorhandler(404)
     def pagina_no_encontrada(error):
-      return render_template('error.html'), 404
+        return render_template('error.html'), 404
+
+    # Comando custom para crear DB
+    @app.cli.command("create-db")
+    @with_appcontext
+    def create_db():
+        db.create_all()
+        click.echo("Base de datos creada.")
 
     return app
