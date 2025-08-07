cab3d4ca5e1738e79cb0a688f9a7159fb8e81d4a
Tactil Pro <roblefelix64@gmail.com>
2025-08-08 01:48:23 +0200

    si

--- app/config.py
+++ app/config.py
@@ -34,7 +34,7 @@ class Config:
     TOKEN_EXPIRATION = 3600  # 1 hora en segundos
 
     # Configuración de correo corregida
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
     MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False").lower() == "true"
